b2461b6172adc08c6f4a4863fefc7fdf26c59313
Tiago Jerônimo <tiago.batmansz@gmail.com>
2023-03-30 21:45:42 -0300

    feat: adiciona componente Footer e estiliza a aplicacao

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from "react";
+import VideoFooter from "./components/footer/VideoFooter"
 import "./Video.css";
+
 
 function Video() {
   const videoRef = useRef(null);
@@ -8,7 +10,7 @@ function Video() {
   function handleStart() {
         
         function isTrue() {
-        videoRef.current.pause();
+        videoRef.current.pause(); 
         setPlay(false);
         }
 
@@ -18,7 +20,7 @@ function Video() {
         }
 
     play ? isTrue() : isFalse();
-  }
+  } 
 
   return (
     <div className="video">
@@ -27,8 +29,9 @@ function Video() {
         ref={videoRef}
         onClick={handleStart}
         loop
-        src="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d"
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
       ></video>
+      <VideoFooter />
     </div>
   );
 }
---
+++ src/pages/components/footer/VideoFooter.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./videoFooter.css";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+
+function VideoFooter() {
+  return (
+    <div className="videoFooter">
+      <div className="videoFooter__text">
+        <h3> @the.jeronimo </h3>
+        <p> Descrição do Vídeo</p>
+        <div className="videoFooter__music">
+          <MusicNoteIcon className="videoFooter__icon" />
+          <div className="videoFooterMusic__text">
+            <p>Titulo da música</p>
+          </div>
+        </div>
+      </div>
+      <img
+        className="videoFooter__record"
+        alt="Icone de um vinil girando"
+        src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/vinil.png"
+      />
+    </div>
+  );
+}
+
+export default VideoFooter;
